0c7b9f282112d4c1aa20b7e5b4f72899dc6cedd6
tommtom9 <tommtom9@live.nl>
2016-04-12 10:01:25 +0200

    Spawning, deleting terrain + Parallax scrolling

---
+++ Gamejam2016/Assets/Scripts/Spawning.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawning : MonoBehaviour {
+
+
+    [SerializeField]
+    GameObject[] stageCollection;
+
+    Transform spawnLocation;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Relatie");
+
+        if (other.gameObject.tag == "SpawnTrigger")
+        {
+            spawnLocation = other.gameObject.transform.parent.Find("Spawnlocation");
+
+            GameObject obj = Instantiate(stageCollection[Random.Range(0, stageCollection.Length)], spawnLocation.position, Quaternion.identity) as GameObject;
+
+            obj.name = "SpawnedStage";
+
+            Destroy(other.gameObject);
+        }
+
+        else if (other.gameObject.tag == "DeleteObject")
+        {
+            Destroy(other.transform.parent.gameObject);
+        }
+    }
+}
